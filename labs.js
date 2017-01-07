a70ed79a29e49855ad3f3cc34dda779a976d91df
UbajakaCJ <ubajaka.chijioke@gmail.com>
2017-01-07 14:21:16 +0100

    Developed the algorithm for findMinMax.js

--- labs.js
+++ labs.js
@@ -3,8 +3,31 @@
 module.exports = {
 
     findMinMax: function (arr) {
+
+        var minNum = arr[0];
+        var maxNum = arr[0];
+        var minMaxList = [];
+        var sameMinMaxList = [];
+
+        for (var i = 1; i < arr.length; i++) {
+            if(arr[i] < minNum){
+                minNum = arr[i];
+            } else if (arr[i] > maxNum) {
+                maxNum = arr[i];
+            };
+        };
+
+        minMaxList.push(minNum);
+        minMaxList.push(maxNum);
+
+        if (minMaxList[0] == minMaxList[1]) {
+            sameMinMaxList.push(minMaxList[0]);
+            return sameMinMaxList;
+        } else {
+            return minMaxList;
+        }
 	// TODO
-	return [];
+	// return [];
     },
 
     fizzBuzz: function (num) {
